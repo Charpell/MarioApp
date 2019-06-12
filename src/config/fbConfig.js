47984529e71e0198fbf827a6560c874370e70722
charpell <charpellumeh@gmail.com>
2019-06-12 11:54:10 +0100

    connecting redux to firebase

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDTkHV7QXyp8i7Csq-oIYci9iNOSbyEOq01",
+  apiKey: "AIzaSyDTkHV7QXyp8i7Csq-oIYci9iNOSbyEOq0",
   authDomain: "net-ninjs-marioplan-101.firebaseapp.com",
-  databaseURL: "https://net-ninjs-marioplan-1301.firebaseio.com",
+  databaseURL: "https://net-ninjs-marioplan-101.firebaseio.com",
   projectId: "net-ninjs-marioplan-101",
   storageBucket: "",
-  messagingSenderId: "386100493720",
+  messagingSenderId: "386100492720",
   appId: "1:386100492720:web:11cbd686d4f38ebe"
 };
 // Initialize Firebase
